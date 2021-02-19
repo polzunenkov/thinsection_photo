fe17cf4fe95d76568f7fbac05c97f6f597014e9b
polzunenkov <gennadiy_mag@mail.ru>
2021-02-19 15:28:41 +1100

    Add lens name

--- thinsection.py
+++ thinsection.py
@@ -2,10 +2,10 @@ import subprocess
 import os	
 import click
 
-def _create_new_path(old_path, thinsection_name):
+def _create_new_path(old_path, thinsection_name, lense):
 	''' Возвращает новое имя для фотографий шлифов с учетом имени thinsection_name
 	'''
-	path_new = os.path.join(old_path, thinsection_name)
+	path_new = os.path.join(old_path, thinsection_name, lense)
 	return path_new
 
 def copy(path):
@@ -17,13 +17,13 @@ def copy(path):
 	
 	subprocess.run(["adb", "pull", "/sdcard/DCIM/Camera", path])		
 
-def rename(path, thinsection_name):
+def rename(old_path, new_path):
 	''' переименовывает в name_folder скопированную папку 
             с телефона из дирректории Camera -стандартной камеры Honor
 	''' 
-	path_old = os.path.join(path, "Camera")
-	path_new = _create_new_path(path, thinsection_name)
-	subprocess.run(["mv", path_old, path_new])		
+	path_old = os.path.join(old_path, "Camera")
+	subprocess.run(["mkdir", "-p", new_path])
+	subprocess.run(["mv", path_old, new_path])		
 	
 
 def del_photo_folder(pattern):
@@ -36,18 +36,6 @@ def del_photo_folder(pattern):
                         f"/sdcard/DCIM/Camera/{pattern}"])		
 
 
-def add_info_obiective(path, thinsection_name): 
-	path_new = _create_new_path(path, thinsection_name)
-	for count, filename in enumerate(os.listdir(path_new)): 
-		dst ="Hostel" + str(count) + ".jpg"
-		src = os.path.join(path_new, filename )
-		dst =os.path.join(path_new, dst )
-		  
-		# rename() function will 
-		# rename all the files 
-		os.rename(src, dst) 
-
-
 
 
 @click.command()
@@ -57,20 +45,23 @@ def add_info_obiective(path, thinsection_name):
               default="IMG_*.jpg")
 @click.option('--thinsection_name', help='name for the thin section', 
               default="thin01")
+@click.option('--lense_name', help='name of the lesne used', 
+              default="x5")
 @click.option('--do_not_remove_from_phone', '-D', 
               help='remove phtos from phone folder ', 
               is_flag=True, default=False)
-def main(path ,pattern, thinsection_name, do_not_remove_from_phone):
+def main(path, pattern, thinsection_name, lense_name, do_not_remove_from_phone):
 	'''
 	Копирует файлы с камеры телефона на компьютер
 	'''
-	path = os.path.normpath(path)
-	
-	click.echo(do_not_remove_from_phone)
-	
-	copy(path)	
-	rename(path, thinsection_name)
-	add_info_obiective(path, thinsection_name)
+	old_path = os.path.normpath(path)
+	new_path = _create_new_path(old_path, thinsection_name, lense_name)
+	click.echo(old_path)
+	click.echo(new_path)
+		
+	copy(old_path)	
+	rename(old_path, new_path)
+	#add_info_obiective(path, thinsection_name)
 	if not do_not_remove_from_phone:
 		del_photo_folder(pattern)
 
